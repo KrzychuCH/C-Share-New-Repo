71802eadc7794d57a7e7957a115d8e916f7d8dd8
Krzychu <tester@gmail.com>
2024-01-22 18:21:49 +0100

    Testy klasy/metody/pól

---
+++ Testy/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Security.Authentication.ExtendedProtection;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Testy
+{
+    internal class Program
+    {
+        public int FirstNumber = 10;
+        public int SecondNumber = 20;
+        public int SumNumber;
+        public string Znak = "*";
+        
+
+      /*  public DoIt(int firstNumber, int secondNumber , int sumNumber)
+        {
+        FirstNumber = firstNumber;
+            SecondNumber = secondNumber;
+            SumNumber = sumNumber;
+            }*/
+        public int Dodawanie()
+        {
+
+            SumNumber = FirstNumber + SecondNumber;
+            return SumNumber;
+        }
+        public int Odejmowanie()
+        {
+
+            SumNumber = FirstNumber - SecondNumber;
+            return SumNumber;
+        }
+        public int Mnożenie()
+        {
+
+            SumNumber = FirstNumber * SecondNumber;
+            return SumNumber;
+        }
+        public int Dzielenie()
+        {
+
+            SumNumber = FirstNumber / SecondNumber;
+            return SumNumber;
+        }
+
+        class Program2
+        {
+            static void Main()
+            {
+                Program dzialanie = new Program();
+
+                int dodawanie = dzialanie.Dodawanie();
+                int odejmowanie = dzialanie.Odejmowanie();
+                int dzielenie = dzialanie.Dzielenie();
+                int mnożenie = dzialanie.Mnożenie();
+                string znak = dzialanie.Znak;
+
+                if (znak == "+")
+                {
+                    Console.WriteLine(dodawanie);
+                } else if (znak == "-")
+                {
+                    Console.WriteLine(odejmowanie);
+                }
+                else if (znak == "/")
+                {
+                    Console.WriteLine(dzielenie);
+                }else if (znak == "*")
+                {
+                    Console.WriteLine(mnożenie);
+                }
+
+            }
+        }
+
+       
+
+
+
+    }
+}
