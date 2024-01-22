d5364fc026b114abdba82b6f38eab8ad71abbc7e
Krzychu <tester@gmail.com>
2024-01-22 21:18:28 +0100

    metody

--- Testy2/Program.cs
+++ Testy2/Program.cs
@@ -2,9 +2,26 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+
+        public string FirstName;
+        public string LastName;
+
+        private DateTime dateOfBirth;
+
+        // poniżej metoda. public void nic nie zwraca. W nawiasie mamy parametry metody
+        public void SetDateOfBirth(DateTime date)
         {
-            Console.WriteLine("Hello, World!");
+            if (date > DateTime.Now) {
+                Console.WriteLine("Invalid Date of Birth");
+            } else
+            {
+                dateOfBirth = date;
+            }
+        }
+        public DateTime GetDateOfBirth()   //  => dateOfBirth;
+        {
+            return dateOfBirth;  // dwa zapisy. Klasyczny i tak jak powyzej uproszczony. 
         }
     }
 }
+// to są metody które powinny byc wywołane z metody main. 
