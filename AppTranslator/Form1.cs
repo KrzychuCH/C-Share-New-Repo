4fcd148e1302fd7e4154a76628eae620b9653379
Krzychu <tester@gmail.com>
2024-02-04 19:12:22 +0100

    Dalsza praca na aplikacją.Mam porblem z poprawnym wyszukiwaniem danych w sowniku lub wprowadzeniem danych

--- AppTranslator/Form1.cs
+++ AppTranslator/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,33 +8,68 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using Newtonsoft.Json;
 
 
 namespace AppTranslator
 {
+
     public partial class Form1 : Form
     {
-        string plWord;
-        string enWord;
-        int flagPl = 0;
-        int flagEn = 0;
-        Dictionary<string, string> PL = new Dictionary<string, string>();
-        Dictionary<string, string> EN = new Dictionary<string, string>();
+        public Dictionary<string, string> PL { get; set; }
+        public Dictionary<string, string> EN { get; set; }
+        private Form2 form2;
+    
         public Form1()
         {
+        
             InitializeComponent();
             textBox1.KeyPress += TextBox1_KeyPress;
+            
+            form2 = new Form2(this); // Przekazujemy referencję do Form1 do konstruktora Form2
+            form2.DataUpdated += Form2_DataUpdated;
+
         }
-
-
-        private void TextBox1_KeyPress(object sender, KeyPressEventArgs e)
+        private void LoadData()
         {
-            if (e.KeyChar == (char)Keys.Enter)
+            
+            PL = new Dictionary<string, string>(); 
+            EN = new Dictionary<string, string>();
+
+            if (File.Exists("dictionary.json"))
             {
-                string enteredText = textBox1.Text;
-                label2.Text = enteredText;
+                string json = File.ReadAllText("dictionary.json");
+                var data = JsonConvert.DeserializeObject<Dictionary<string, string>[]>(json);
+                PL = data[0];
+                EN = data[1];
             }
         }
+        private void Form2_DataUpdated(object sender, EventArgs e)
+        {
+            // Tutaj odświeżamy dane w Form1, np. ponownie wczytujemy słownik
+            LoadData();
+        }
+
+        private void TextBox1_KeyPress(object sender, KeyPressEventArgs e)
+       {
+             if (e.KeyChar == (char)Keys.Enter)
+             {
+                /* string enteredText = textBox1.Text;
+                 string translation = GetTranslation(enteredText); // Pobierz tłumaczenie
+                 label2.Text = translation; // */
+                HandleEnterKeyPress();
+
+            }
+           
+        }
+        private void HandleEnterKeyPress()
+        {
+            string enteredText = textBox1.Text;
+            string translation = GetTranslation(enteredText); // Pobierz tłumaczenie
+            label2.Text = translation;
+        }
+
 
         private void label1_Click(object sender, EventArgs e)
         {
@@ -42,9 +78,34 @@ namespace AppTranslator
 
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
-            // Ta metoda zostanie wywołana za każdym razem, gdy tekst w polu tekstowym zostanie zmieniony
+            /*string enteredText = textBox1.Text;
+            if (enteredText != "") // Upewnij się, że wpisano coś do textBox1
+            {
+                string translation = GetTranslation(enteredText); // Pobierz tłumaczenie
+                label2.Text = translation; // Wyświetl tłumaczenie w label2
+            }
+            string enteredText = textBox1.Text;
+            if (!string.IsNullOrEmpty(enteredText))
+            {
+                string translation = GetTranslation(enteredText);
+                label2.Text = translation;
+            }
+            else
+            {
+                label2.Text = string.Empty; // Wyczyść label2, jeśli pole tekstowe jest puste
+            }*/
         }
-
+        private string GetTranslation(string enteredText)
+        {
+            if (form2.PL.ContainsKey(enteredText))
+            {
+                return form2.PL[enteredText]; // Zwróć tłumaczenie ze słownika
+            }
+            else
+            {
+                return "Brak tłumaczenia"; // Zwróć wiadomość o braku tłumaczenia
+            }
+        }
         private void label2_Click(object sender, EventArgs e)
         {
 
@@ -52,8 +113,12 @@ namespace AppTranslator
 
         private void button1_Click(object sender, EventArgs e)
         {
-            Form2 form2 = new Form2(); // Tworzymy nową instancję nowego formularza
-            form2.ShowDialog(); // Wyświetlamy nowe okno jako okno dialogowe
+            Form2 form2 = new Form2(this); // Przekazanie instancji Form1 do konstruktora Form2
+            form2.ShowDialog(); // Wyświetlenie Form2
+
+            string enteredText = form2.plWord; // Pobranie tłumaczenia z Form2
+            label2.Text = enteredText;
+            
         }
 
     }
--- AppTranslator/Form2.cs
+++ AppTranslator/Form2.cs
@@ -5,26 +5,153 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using Newtonsoft.Json;
 
 namespace AppTranslator
 {
     public partial class Form2 : Form
     {
+        public string plWord;
+        string enWord;
+        int flagPl = 0;
+        int flagEn = 0;
+         public  Dictionary<string, string> PL = new Dictionary<string, string>();
+         public  Dictionary<string, string> EN = new Dictionary<string, string>();
+        public event EventHandler DataUpdated;
+
+        private Form1 parentForm;
+        private Form1 form1;
+        public Form2(Form1 parent)
+
+        {
+            InitializeComponent();
+            parentForm = parent;
+        }
         public Form2()
         {
             InitializeComponent();
+           
+        }
+    
+        public void GetDatePl()
+        {
+
+            Regex regexPl = new Regex(@"[\W\d\s]");
+            bool ismatch = regexPl.IsMatch(plWord);
+            if (ismatch == true)
+            {
+                MessageBox.Show("Wprowadzony tekst: " + plWord + " - Słowo nie powinno zawierać cyfr,biłaych znaków lub znaków specjalnych jak np. (+,@,#)");
+                flagPl = 1;
+                return;
+            }
+            else
+            {
+                plWord = plWord.ToUpper();
+            }
+        }
+        public void GetDateEn()
+        {
+
+            Regex regexEN = new Regex(@"[\W\d\s]");
+            bool ismatch = regexEN.IsMatch(enWord);
+            if (ismatch == true)
+            {
+                MessageBox.Show("Wprowadzony tekst: " + enWord + " - Słowo nie powinno zawierać cyfr,biłaych znaków lub znaków specjalnych jak np. (+,@,#)");
+                flagEn = 1;
+                return;
+            }
+            else
+            {
+                enWord = enWord.ToUpper();
+            }
+
+        }
+        public void Dict()
+        {
+
+            if (!PL.ContainsKey(plWord))
+                PL.Add(plWord, enWord);
+            if (!EN.ContainsKey(enWord))
+                EN.Add(enWord, plWord);
+
+        }
+        private void UpdateDictionary()
+        {
+            // Kod aktualizacji słownika
+
+            // Wywołanie zdarzenia informującego o aktualizacji danych
+            DataUpdated?.Invoke(this, EventArgs.Empty);
+        }
+        private void LoadData()
+        {
+            if (File.Exists("dictionary.json"))
+            {
+                string json = File.ReadAllText("dictionary.json");
+                var data = JsonConvert.DeserializeObject<Dictionary<string, string>[]>(json);
+                PL = data[0];
+                EN = data[1];
+            }
+        }
+        private void SaveData()
+        {
+            var data = new Dictionary<string, string>[] { PL, EN };
+            string json = JsonConvert.SerializeObject(data);
+            File.WriteAllText("dictionary.json", json);
         }
 
         private void Form2_Load(object sender, EventArgs e)
         {
-
+            LoadData();
+            string enteredText = parentForm.textBox1.Text; // Pobierz wpisane słowo z Form1
+            if (PL.ContainsKey(enteredText))
+            {
+                string translation = PL[enteredText]; // Pobierz tłumaczenie
+                parentForm.label2.Text = translation; // Przypisz tłumaczenie do label2 w Form1
+            }
         }
 
         private void label1_Click(object sender, EventArgs e)
         {
 
         }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+      
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            plWord = textBox1.Text;
+            enWord = textBox2.Text;
+
+            GetDatePl();
+            GetDateEn();
+
+            if (flagPl == 0 && flagEn == 0)
+            {
+                Dict();
+                SaveData();
+                DataUpdated?.Invoke(this, EventArgs.Empty);
+                MessageBox.Show("Słowa zostały dodane do słownika.");
+            }
+
+            string enteredText = textBox1.Text;
+            parentForm.label2.Text = enteredText;
+
+            plWord = textBox2.Text;
+        }
+
+
     }
 }
