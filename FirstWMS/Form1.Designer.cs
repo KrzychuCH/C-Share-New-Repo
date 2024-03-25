26664ae843d8897e00a8118a4e7c2579e8e0bf39
Krzychu <tester@gmail.com>
2024-03-25 23:26:49 +0100

    logowanie do wms

---
+++ FirstWMS/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace FirstWMS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.logged = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.CausesValidation = false;
+            this.label1.Font = new System.Drawing.Font("Arial", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(195, 9);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "LOGIN";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2.CausesValidation = false;
+            this.label2.Font = new System.Drawing.Font("Arial", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(195, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "HASŁO";
+            // 
+            // logged
+            // 
+            this.logged.BackColor = System.Drawing.Color.LawnGreen;
+            this.logged.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.logged.CausesValidation = false;
+            this.logged.Cursor = System.Windows.Forms.Cursors.Default;
+            this.logged.Font = new System.Drawing.Font("Arial", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.logged.Location = new System.Drawing.Point(354, 170);
+            this.logged.Name = "logged";
+            this.logged.Size = new System.Drawing.Size(106, 29);
+            this.logged.TabIndex = 2;
+            this.logged.Text = "Logowanie";
+            this.logged.UseVisualStyleBackColor = false;
+            this.logged.Click += new System.EventHandler(this.logged_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.textBox1.Location = new System.Drawing.Point(12, 37);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(448, 32);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.CausesValidation = false;
+            this.textBox2.Location = new System.Drawing.Point(12, 115);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(448, 32);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(472, 211);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.logged);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Calibri", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.Name = "Form1";
+            this.Text = "Logowanie";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button logged;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ FirstWMS/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FirstWMS
+{
+    public partial class Form1 : Form
+    {
+
+        string logg;
+        string pass;
+        int res;
+        string message;
+        public Form1()
+        {
+            InitializeComponent();
+     
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            logg = textBox1.Text;
+        }
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            pass = textBox2.Text;
+        }
+
+        private void logged_Click(object sender, EventArgs e)
+        {
+            {
+                string connectionString = "Server=DESKTOP-T0QFA56;Database=AdventureWorks;Trusted_Connection=True";
+                string procedureName = "LoginUsers";
+
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    // Tworzenie obiektu SqlCommand dla procedury
+                    SqlCommand command = new SqlCommand(procedureName, connection);
+                    command.CommandType = CommandType.StoredProcedure;
+
+                    // Dodanie parametrów procedury
+                    command.Parameters.AddWithValue("@Login",logg);
+                    command.Parameters.AddWithValue("@Pass",pass);
+
+                    command.Parameters.Add("@msg", SqlDbType.NVarChar, 254).Direction = ParameterDirection.Output;
+                    command.Parameters.Add("@res", SqlDbType.Int).Direction = ParameterDirection.Output;
+
+                    /*
+                                        // Dodanie parametru wyjściowego do przechwycenia komunikatu z procedury
+                                        SqlParameter outputParameter = new SqlParameter("@msg",SqlDbType.NVarChar,254);
+                                        outputParameter.Direction = ParameterDirection.Output;
+                                        command.Parameters.Add(outputParameter);
+
+                                        // Dodanie parametru wyjściowego do przechwycenia wyniku autoryzacji z procedury
+                                        SqlParameter outputParameter2 = new SqlParameter("@res",SqlDbType.Int);
+                                        outputParameter2.Direction = ParameterDirection.Output;
+                                        command.Parameters.Add(outputParameter2);*/
+
+                    try
+                    {
+                        connection.Open();
+
+
+                        command.ExecuteNonQuery();
+                        /*
+                        // Pobranie komunikatu z procedury
+                        message = command.Parameters["@msg"].Value.ToString();
+                        // Pobranie wyniku autoryzacji z procedury
+                        res = (int)command.Parameters["@res"].Value;
+
+                        // Wyświetlenie komunikatu
+                        MessageBox.Show(message);*/
+                        string message = command.Parameters["@msg"].Value.ToString();
+                        int result = (int)command.Parameters["@res"].Value;
+
+                        if (!string.IsNullOrEmpty(message))
+                        {
+                            MessageBox.Show(message);
+                        }
+                        // Ustawienie wartości zmiennej res
+                        res = result;
+
+                    }
+                    catch (Exception ex)
+                    {
+                        // Obsługa wyjątków
+                        MessageBox.Show("Wystąpił błąd: " + ex.Message);
+                    }
+                }
+            }
+
+        }
+    }
+}
